cb9b7813d593c739eb90281a933d44ecbf133dc1
Tchy <tchy258@yahoo.cl>
2022-06-09 14:22:12 -0400

    Se agrego el esquema basico de movimiento en la funcion on_key

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ class Controller:
         self.cameraThetaAngle = 0
         self.strafeSpeed = 200./1280.
         self.forwardSpeed = 200./1280.
+        self.currentStrafeSpeed = 0
+        self.currentForwardSpeed = 0
         self.camPos=[0.,0.,0.]
         self.camFront=[1.,0.,0.]
         self.camUp=[0.,0.,1.]
@@ -34,12 +36,30 @@ def on_key(window, key, scancode, action, mods):
 
     global controller
 
-    movementSpeed=[0.,0.,0.]
-
+    movementSpeed=np.array([0.,0.,0.])
+    forwardSpeed=np.array([0.,0.,0.])
+    strafeSpeed=np.array([0.,0.,0.])
+    if key==glfw.KEY_LEFT_SHIFT and action==glfw.PRESS:
+        controller.currentForwardSpeed*=2
+        controller.currentStrafeSpeed*=2
     if key==glfw.KEY_W and action==glfw.PRESS:
-        forwardSpeed=np.array(controller.camFront)/np.linalg(np.array(controller.camFront))
-        forwardSpeed*=controller.forwardSpeed
+        forwardSpeed=np.array(controller.camFront)/np.linalg.norm(np.array(controller.camFront))
+        forwardSpeed*=controller.currentForwardSpeed
     if key==glfw.KEY_S and action==glfw.PRESS:
-        forwardSpeed=np.array(controller.camFront)/np.linalg(np.array(controller.camFront))
-        forwardSpeed*=-controller.forwardSpeed
-        
+        forwardSpeed=np.array(controller.camFront)/np.linalg.norm(np.array(controller.camFront))
+        forwardSpeed*=-controller.currentForwardSpeed
+    if key==glfw.KEY_A and action==glfw.PRESS:
+        strafeSpeed=np.array(controller.camFront)/np.linalg.norm(np.array(controller.camFront))
+        strafeSpeed*=controller.currentStrafeSpeed
+        rotacionIzquierda=tr.rotationZ(-np.pi/2)
+        strafeSpeed=tr.matmul(rotacionIzquierda,strafeSpeed)
+    if key==glfw.KEY_D and action==glfw.PRESS:
+        strafeSpeed=np.array(controller.camFront)/np.linalg.norm(np.array(controller.camFront))
+        strafeSpeed*=controller.currentStrafeSpeed
+        rotacionIzquierda=tr.rotationZ(np.pi/2)
+        strafeSpeed=tr.matmul(rotacionIzquierda,strafeSpeed)
+    movementSpeed=forwardSpeed+strafeSpeed
+    if not np.all(movementSpeed==0):
+        movementSpeed/=np.linalg.norm(movementSpeed)
+        movementSpeed=movementSpeed.tolist()
+        controller.camPos+=movementSpeed
