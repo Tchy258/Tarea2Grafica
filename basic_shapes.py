bc2306148251093428161a350aa8a96cd5a844ff
Nicolás Escobar Zarzar <tchy258@yahoo.cl>
2022-06-12 02:23:16 -0400

    Arreglo de orientacion de texturas en el cubo y creacion de la funcion createTexturePyramid

--- basic_shapes.py
+++ basic_shapes.py
@@ -32,15 +32,15 @@ def createTextureCube():
 
         # X+
         0.5, -0.5, -0.5, 0, 1,
-        0.5,  0.5, -0.5, 1, 1,
+        0.5,  0.5, -0.5, 0, 0,
         0.5,  0.5,  0.5, 1, 0,
-        0.5, -0.5,  0.5, 0, 0,
+        0.5, -0.5,  0.5, 1, 1,
 
         # X-
         -0.5, -0.5, -0.5, 0, 1,
-        -0.5,  0.5, -0.5, 1, 1,
+        -0.5,  0.5, -0.5, 0, 0,
         -0.5,  0.5,  0.5, 1, 0,
-        -0.5, -0.5,  0.5, 0, 0,
+        -0.5, -0.5,  0.5, 1, 1,
 
         # Y+
         -0.5,  0.5, -0.5, 0, 1,
@@ -59,10 +59,40 @@ def createTextureCube():
     # We have a triangle every 3 indices specified
     indices = [
         0, 1, 2, 2, 3, 0,  # Z+
-        7, 6, 5, 5, 4, 7,  # Z-
+        7, 4, 5, 5, 6, 7,  # Z-
         8, 9, 10, 10, 11, 8,  # X+
         15, 14, 13, 13, 12, 15,  # X-
         19, 18, 17, 17, 16, 19,  # Y+
         20, 21, 22, 22, 23, 20]  # Y-
+
+    return Shape(vertices, indices)
+
+def createTexturePyramid():
+
+    # Defining locations and texture coordinates for each vertex of the shape
+    vertices = [
+        # Y-
+        -0.5, -0.5, -0.5, 0, 1,
+        0.5, -0.5, -0.5, 1, 1,
+        0.5, -0.5,  0.5, 0, 1,
+        -0.5, -0.5,  0.5, 1, 1,
+
+        # Y+
+        0.0,  0.5, 0.0, 0.5, 0,
+
+        # Y-
+        0.5, -0.5,  0.5, 1, 0,
+        -0.5, -0.5,  0.5, 0, 0
+    ]
+
+    # Defining connections among vertices
+    # We have a triangle every 3 indices specified
+    indices = [
+        0, 1, 5, 5, 6, 0,  # Y-
+        0, 4, 1, #Z-
+        1, 4, 2, #X+
+        2, 4, 3,
+        3, 4 ,0
+    ]
 
     return Shape(vertices, indices)
