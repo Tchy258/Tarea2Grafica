ef80a9f2cd7cfcbab32c5907848872c300750d4e
Nicolás Escobar Zarzar <tchy258@yahoo.cl>
2022-06-11 21:19:24 -0400

    Nuevo archivo para la clase controller

---
+++ controller.py
@@ -0,0 +1,22 @@
+import constants
+import numpy as np
+
+#Clase para manejar la camara y movimiento
+class Controller:
+    def __init__(self):
+        self.cameraPhiAngle = 0
+        self.cameraThetaAngle = 0
+        self.camBaseSpeed=0.5
+        self.camSpeed= 0.5
+        self.camPos=np.array([0.,0.,0.])
+        self.camFront=np.array([1.,0.,0.])
+        self.camUp=np.array([0.,1.,0.])
+        self.IsOrtho=False
+        self.view = [
+            self.camPos,
+            self.camPos+self.camFront,
+            self.camUp
+        ]
+        self.cursorShouldHide=False
+        self.mouseX=constants.SCREEN_WIDTH/2.
+        self.mouseY=constants.SCREEN_HEIGHT/2.
