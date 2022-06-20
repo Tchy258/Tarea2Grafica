d1e46140055cb023b9d96457fe452ff81dbdaa23
Nicolás Escobar Zarzar <tchy258@yahoo.cl>
2022-06-20 02:14:46 -0400

    Ajuste posición inicial

--- controller.py
+++ controller.py
@@ -8,7 +8,7 @@ class Controller:
         self.cameraThetaAngle = 0
         self.camBaseSpeed=0.5
         self.camSpeed= 0.7
-        self.camPos=np.array([9,0.2,6.0])
+        self.camPos=np.array([-0.7,0.2,14.7])
         self.camFront=np.array([1.,0.,0.])
         self.camUp=np.array([0.,1.,0.])
         self.IsOrtho=False
