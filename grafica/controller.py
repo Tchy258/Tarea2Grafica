1f0cbf229c5303ae3561f4aecce0be63e217404b
Nicolás Escobar Zarzar <tchy258@yahoo.cl>
2022-07-04 01:51:32 -0400

    Se añadió la clase spotlight para las luces y la clase Car para manipular el auto (WIP)

--- grafica/controller.py
+++ grafica/controller.py
@@ -1,4 +1,4 @@
-import constants
+import grafica.constants as constants
 import numpy as np
 
 #Clase para manejar la camara y movimiento
@@ -9,7 +9,7 @@ class Controller:
         self.camBaseSpeed=0.5
         self.camSpeed= 0.7
         self.camPos=np.array([-0.7,0.2,14.7])
-        self.camFront=np.array([1.,0.,0.])
+        self.camFront=np.array([0.,1.,0.])
         self.camUp=np.array([0.,1.,0.])
         self.IsOrtho=False
         self.view = [
@@ -20,3 +20,24 @@ class Controller:
         self.cursorShouldHide=True
         self.mouseX=constants.SCREEN_WIDTH/2.
         self.mouseY=constants.SCREEN_HEIGHT/2.
+        self.flashlightOn=False
+
+#Clase para contener todos los atributos de las luces
+class Spotlight:
+    def __init__(self):
+        self.ambient = np.array([0,0,0])
+        self.diffuse = np.array([0,0,0])
+        self.specular = np.array([0,0,0])
+        self.constant = 0
+        self.linear = 0
+        self.quadratic = 0
+        self.position = np.array([0,0,0])
+        self.direction = np.array([0,0,0])
+        self.cutOff = 0
+        self.outerCutOff = 0
+
+class Car:
+    def __init__(self):
+        self.Pos=np.array([-4.71922016 , 0.2, -1.44114551])
+        self.lights=False
+        self.speed=2
